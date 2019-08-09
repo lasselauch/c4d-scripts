d8f9355182e182e5e8ef513a86f82685661b4a02
Lasse Lauch <44460746+lasselauch@users.noreply.github.com>
2019-08-09 12:08:13 +0200

    Update/Upload quickSEL

--- quickSel/quickSel.py
+++ quickSel/quickSel.py
@@ -2,86 +2,42 @@
 Copyright: Lasse Lauch
 Written for Cinema 4D R20.059
 
-Name-US:quickSel_v0.01
-Description-US:A quick Selection-Menu for last selected Objects. Add to Hotkey (i.e. CTRL+TAB)
+Name-US:quickSEL
+Description-US:A quick Selection-Menu for last selected Objects/Tags.\nAdd to Hotkey (i.e. Shift+TAB)
 Idea by Lars Jandel
-Last Modified Date: 07/08/2019
+Last Modified Date: 09/08/2019
 """
+
+import c4d
+import os
 
 PLUGIN_ID = 1053141
 
-import c4d
-
-def GetNextElement(op):
-    if not op:
-        return
-
-    if op.GetDown():
-        return op.GetDown()
-
-    while (not op.GetNext()) and op.GetUp():
-        op = op.GetUp()
-
-    return op.GetNext()
-
-def set_pluginData(sel, docData):
+def set_pluginData(sel, docData, two_selected=False):
     myBC = c4d.BaseContainer()
-
-    bc1 = c4d.BaseContainer()
-    bc2 = c4d.BaseContainer()
-    for i, obj in enumerate(reversed(sel)):
-        icon = '&i%s&' % (obj.GetType())
-        name = icon + obj.GetName()
-        unique_id = obj.GetGUID()
-        bc1.SetString(i, name)
-        bc2.SetData(i, unique_id)
-
-    myBC.SetData(0, bc1)
-    myBC.SetData(1, bc2)
+    for i, obj in enumerate(sel):
+        print obj
+        if two_selected:
+            myBC.SetLink(1000+i, obj)
+        else:
+            myBC.SetLink(i, obj)
 
     docData.SetData(PLUGIN_ID, myBC)
 
-def get_pluginData(docData):
+def popupMenu(docData):
     if docData is not None:
         pluginData = docData.GetData(PLUGIN_ID)
-
-    names = list()
-    unique_ids = list()
-
-    if pluginData is None:
-        c4d.gui.MessageDialog("Please, select some Objects to work with.")
-        return None, None
-    
-    # Loop through both Containers 0 / 1
-    for i, bc in pluginData:
-        # print i, bc
-        #Loop through individual Containers (name and unique_id)
-        for k, v in bc:
-            if isinstance(v, basestring):
-                names.append(v)
-            if isinstance(v, long):
-                unique_ids.append(v)
-
-    return names, unique_ids
-
-def popupMenu(liste):
-    if not liste:
-        return None
+        if not pluginData:
+            return None
 
     menu = c4d.BaseContainer()
-    for i, name in enumerate(liste):
-        menu.InsData(c4d.FIRST_POPUP_ID+i, name)
+    for i, (k, obj) in enumerate(pluginData):
+        icon = '&i%s&' % (obj.GetType())
+        name =  obj.GetName()
+        menu.InsData(c4d.FIRST_POPUP_ID+i, icon+name)
 
     result = c4d.gui.ShowPopupDialog(None, menu, c4d.MOUSEPOS, c4d.MOUSEPOS, c4d.POPUP_CENTERVERT|c4d.POPUP_CENTERHORIZ|c4d.POPUP_EXECUTECOMMANDS)
     return result - c4d.FIRST_POPUP_ID
-
-def FindObject(unique_id):
-    obj = doc.GetFirstObject()
-    while(obj is not None):
-        if obj.GetGUID() == unique_id:
-            return obj
-            break
-        obj = GetNextElement(obj)
 
 def ToggleSetBit(a, b):
     a_sel, b_sel = False, False
@@ -90,42 +46,64 @@ def ToggleSetBit(a, b):
     if b.GetBit(c4d.BIT_ACTIVE): b_sel = True
 
     if a_sel:
-        a.DelBit(c4d.BIT_ACTIVE)
-        b.SetBit(c4d.BIT_ACTIVE)
+        doc.AddUndo(c4d.UNDOTYPE_BITS, a)
+        doc.SetSelection(b, c4d.SELECTION_NEW)
     if b_sel:
-        b.DelBit(c4d.BIT_ACTIVE)
-        a.SetBit(c4d.BIT_ACTIVE)
+        doc.AddUndo(c4d.UNDOTYPE_BITS, b)
+        doc.SetSelection(a, c4d.SELECTION_NEW)
+
     if a_sel and b_sel:
+        doc.AddUndo(c4d.UNDOTYPE_BITS, a)
         a.DelBit(c4d.BIT_ACTIVE)
+        doc.AddUndo(c4d.UNDOTYPE_BITS, b)
         b.DelBit(c4d.BIT_ACTIVE)
-        a.SetBit(c4d.BIT_ACTIVE)
+        b.SetBit(c4d.BIT_ACTIVE)
+
     if not a_sel and not b_sel:
-        a.SetBit(c4d.BIT_ACTIVE)
+        doc.AddUndo(c4d.UNDOTYPE_BITS, b)
+        b.SetBit(c4d.BIT_ACTIVE)
 
 def main():
+    doc.StartUndo()
     docData = doc.GetDataInstance()
-    sel = doc.GetActiveObjects(c4d.GETACTIVEOBJECTFLAGS_CHILDREN | c4d.GETACTIVEOBJECTFLAGS_SELECTIONORDER)
+
+    if docData is not None:
+        pluginData = docData.GetData(PLUGIN_ID)
+
+    sel = doc.GetSelection()
 
     if sel:
-        if len(sel) > 1:
+        if len(sel) == 1:
+            pass
+
+        if len(sel) > 2:
+            if pluginData:
+                pluginData.RemoveIndex(1000)
+                pluginData.RemoveIndex(1001)
             set_pluginData(sel, docData)
 
-    names, unique_ids = get_pluginData(docData)
-    if not names:
-        return
+        if len(sel) == 2:
+            set_pluginData(sel, docData, two_selected=True)
+        
+        if pluginData:
+            a = pluginData.GetLink(1000, doc)
+            b = pluginData.GetLink(1001, doc)
     
-    if len(names) == 2:
-        a = FindObject(unique_ids[0])
-        b = FindObject(unique_ids[1])
-        ToggleSetBit(a, b)
-        c4d.EventAdd()
-        return
+            if a and b:
+                if len(sel)<=2:
+                    ToggleSetBit(a, b)
+                    c4d.EventAdd()
+                    doc.EndUndo()
+                    return
 
-    selection = popupMenu(names)
-    if selection >= 0:
-        obj = FindObject(unique_ids[selection])
-        doc.SetSelection(obj, c4d.SELECTION_NEW)
-        c4d.EventAdd()
+    if len(sel) != 2:
+        selection = popupMenu(docData)
+        if selection >= 0:
+                hero = pluginData.GetLink(selection, doc)
+                if hero:
+                    doc.SetSelection(hero, c4d.SELECTION_NEW)
+                    c4d.EventAdd()
+                    doc.EndUndo()
 
 if __name__=='__main__':
     main()
